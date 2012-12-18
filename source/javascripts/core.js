902e9e449123488d1a249bf04d9ab60347f4c214
Chuck Lauer Vose <vosechu@gmail.com>
2012-12-18 13:31:29 -0800

    add collapsable faq entries

--- source/javascripts/core.js
+++ source/javascripts/core.js
@@ -1,1 +1,17 @@
- 
+$(function() {
+  $('.collapsable p:odd').hide();
+  var previous_q = null;
+  $('.collapsable p').each(function () {
+    if ($('strong', this).length > 0) {
+      // Store the question so that we can bind to it later
+      previous_q = $(this);
+      previous_q.css('text-decoration', 'underline');
+    }
+    else {
+      var _this = this;
+      previous_q.bind('click', function () {
+        $(_this).slideToggle();
+      });
+    }
+  });
+});
