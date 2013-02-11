c460a4233e07048277617ddc6fd3385fab06eeab
Chuck Lauer Vose <vosechu@gmail.com>
2013-02-10 17:00:04 -0800

    changes from erica

--- source/javascripts/core.js
+++ source/javascripts/core.js
@@ -1,17 +1,18 @@
-$(function() {
-  $('.collapsable p:odd').hide();
-  var previous_q = null;
-  $('.collapsable p').each(function () {
-    if ($('strong', this).length > 0) {
-      // Store the question so that we can bind to it later
-      previous_q = $(this);
-      previous_q.css('text-decoration', 'underline');
-    }
-    else {
-      var _this = this;
-      previous_q.bind('click', function () {
-        $(_this).slideToggle();
-      });
-    }
-  });
-});
+// $(function() {
+//   // $('.collapsable p:odd').hide();
+//   var previous_q = null;
+//   $('.collapsable p, .collapsable ul, .collapsable li').each(function () {
+//     if ($('strong', this).length > 0) {
+//       // Store the question so that we can bind to it later
+//       previous_q = $(this);
+//       previous_q.css('text-decoration', 'underline');
+//     }
+//     else {
+//       var _this = this;
+//       previous_q.bind('click', function () {
+//         console.log(_this);
+//         $(_this).slideToggle();
+//       });
+//     }
+//   });
+// });
