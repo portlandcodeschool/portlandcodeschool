57dce6a066a350d3fba2c79445defe2eda5f67b6
Cris Kelly <cris.kelly@gmail.com>
2013-11-04 23:20:09 -0800

    Fixed FAQ page

--- source/javascripts/core.js
+++ source/javascripts/core.js
@@ -16,3 +16,21 @@
 //     }
 //   });
 // });
+
+// hide/reveal the faq answers
+
+$(document).ready(function() {
+
+  $('.answer').hide();
+
+  $('.question h4 a').click(function(e) {
+    e.preventDefault();
+    $(this).parent().next().slideToggle();
+  });
+
+
+
+
+});
+
+
