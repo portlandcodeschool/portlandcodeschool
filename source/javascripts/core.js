aa4bf7a0763e4aa2ad372b9aa8411ebb98660ecf
Timothy Winters <timothy@timothywinters.com>
2014-01-03 14:46:01 -0800

    updated classes and FAQ pages

--- source/javascripts/core.js
+++ source/javascripts/core.js
@@ -1,3 +1,22 @@
+// $(function() {
+//   // $('.collapsable p:odd').hide();
+//   var previous_q = null;
+//   $('.collapsable p, .collapsable ul, .collapsable li').each(function () {
+//     if ($('strong', this).length > 0) {
+//       // Store the question so that we can bind to it later
+//       previous_q = $(this);
+//       previous_q.css('text-decoration', 'underline');
+//     }
+//     else {
+//       var _this = this;
+//       previous_q.bind('click', function () {
+//         console.log(_this);
+//         $(_this).slideToggle();
+//       });
+//     }
+//   });
+// });
+
 // hide/reveal the faq answers
 
 $(document).ready(function() {
@@ -13,4 +32,3 @@ $(document).ready(function() {
 
 
 });
-
