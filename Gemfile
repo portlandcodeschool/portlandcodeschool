96e899f9d46359f57ab898835617eb2593770e2f
Shawna C Scott <shawnatyger@gmail.com>
2014-01-02 18:47:57 -0800

    Update config to support relative links

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 # the following line to use "https"
 source 'http://rubygems.org'
 
-gem "middleman", "~> 3.2.0"
+gem "middleman"
 gem "middleman-blog", "~> 3.4.1"
 gem "middleman-syntax"
 gem "middleman-gh-pages"
--- config.rb
+++ config.rb
@@ -51,6 +51,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :relative_links, true
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -63,8 +65,8 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  # set :http_prefix, "/pcsredesign"
 end
