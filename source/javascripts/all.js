30d8bfb48bee41fa2c4a672f7da48b0c05651f65
Timothy Winters <timothy@timothywinters.com>
2014-01-10 17:18:02 -0800

    Updated FAQ page and added js from old FAQ

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,16 @@
 // require tree .
+
+
+$(document).ready(function() {
+
+  $('.answer').hide();
+
+  $('.question h4 a').click(function(e) {
+    e.preventDefault();
+    $(this).parent().next().slideToggle();
+  });
+
+
+
+
+});
