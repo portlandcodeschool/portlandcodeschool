df3b04e482ebcb3499ef7c389b2c970f3ea2eaf1
Cris Kelly <cris.kelly@gmail.com>
2014-10-28 12:57:27 -0700

    edited js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,23 +6,15 @@
 
 $(document).ready(function() {
 
+  // FAQ page answer hide
+  $('.answer').hide();
+
   // FAQ page answer toggling
   $('.question h4 a').click(function(e) {
     e.preventDefault();
     $(this).parent().next().slideToggle();
   });
 
-  $('.primer-dates li li').css('display','none');
-
-  // Primer date toggle function
-  function primerDateShow() {
-
-  }
-
-    // Primer page date toggling
-  $('.primer-dates li').on(function(e) {
-    sdf
-  });
 });
 
 $('.mc-news').on('click', function() {
