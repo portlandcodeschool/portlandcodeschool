91225a5fdfbb247e888bf51f6599b6997d393486
Cris Kelly <cris.kelly@gmail.com>
2015-02-18 09:57:32 -0800

    changed everything

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -15,6 +15,12 @@ $(document).ready(function() {
     $(this).parent().next().slideToggle();
   });
 
+  var options = { $AutoPlay: true };
+  var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+
+  // parallax
+  // $('.parallax-window').parallax({imageSrc: '/images/banner-pdx.png'});
+
 });
 
 $('.mc-news').on('click', function() {
@@ -24,3 +30,5 @@ $('.mc-news').on('click', function() {
 $('.mc-mobile').on('click', function() {
   ga('send', 'event', 'button', 'click', 'mc-mobile');
 });
+
+
